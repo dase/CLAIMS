657092edc010c5eca0cfaa43b5b41ee295f24f0b
Li Wang <wangli1426@gmail.com>
2013-11-17 19:12:24 +0800

    from Robert<Centos>

--- LogicalQueryPlan/Filter.h
+++ LogicalQueryPlan/Filter.h
@@ -12,14 +12,33 @@
 #include "../iterator/FilterIterator.h"
 class Filter:public LogicalOperator {
 public:
+	class Condition{
+	public:
+		friend class Filter;
+//		typedef FilterIterator::AttributeComparator::comparison comparison;
+		Condition(){}
+		~Condition();
+		void add(const Attribute& attr,const FilterIterator::AttributeComparator::comparison& com,const void*const& const_value);
+
+	private:
+		unsigned getCompaisonNumber()const;
+	private:
+		std::vector<Attribute> attribute_list_;
+		std::vector<FilterIterator::AttributeComparator::comparison> comparison_list_;
+		std::vector<void*> const_value_list_;
+	};
 	Filter(std::vector<FilterIterator::AttributeComparator> ComparatorList,LogicalOperator* child );
+	Filter(const Condition& condition, LogicalOperator*  child);
 	virtual ~Filter();
 	Dataflow getDataflow();
 	BlockStreamIteratorBase* getIteratorTree(const unsigned& blocksize);
 private:
-	bool couldHashPruned(unsigned partition_id,const DataflowPartitionDescriptor&,const Dataflow& dataflow);
+	bool couldHashPruned(unsigned partition_id,const DataflowPartitionDescriptor&)const;
+	float predictSelectivity()const;
+	void generateComparatorList(const Dataflow&);
 private:
 	LogicalOperator* child_;
+	Condition condition_;
 	vector<FilterIterator::AttributeComparator> comparator_list_;
 
 };
