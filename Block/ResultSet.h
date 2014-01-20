939ecd44191d458c680fd2f6bca91f54098cb07d
Li Wang <wangli1426@gmail.com>
2014-01-20 20:08:20 +0800

    add ResultSet data structure which is used to hold the result data.

---
+++ Block/ResultSet.h
@@ -0,0 +1,15 @@
+/*
+ * ResultSet.h
+ *
+ *  Created on: Jan 20, 2014
+ *      Author: wangli
+ */
+
+#ifndef RESULTSET_H_
+#define RESULTSET_H_
+
+#include "DynamicBlockBuffer.h"
+
+typedef DynamicBlockBuffer ResultSet;
+
+#endif /* RESULTSET_H_ */
