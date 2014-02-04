f0816ec1496cb6858541762a66684d7b5b2ae2d9
Li Wang <wangli1426@gmail.com>
2014-02-04 21:13:04 +0800

    add two special attributes, namely ANY and NULL.

--- Catalog/Attribute.h
+++ Catalog/Attribute.h
@@ -11,6 +11,9 @@
 
 #include "../data_type.h"
 #include "../ids.h"
+#define ATTRIBUTE_NULL -1
+#define ATTRIBUTE_ANY -2
+
 struct Attribute
 {
 	Attribute(TableID tableid,unsigned pos,const std::string& name, data_type type, unsigned size = 0)
@@ -23,8 +26,8 @@ struct Attribute
 	}
 	Attribute(const Attribute& att){
 		table_id_=att.table_id_;
-		if(att.table_id_!=-1){
-			attrName=att.attrName;
+		attrName=att.attrName;
+		if(att.table_id_<ATTRIBUTE_ANY){
 			attrType=new column_type(*att.attrType);
 			index=att.index;
 		}
@@ -32,19 +35,34 @@ struct Attribute
 	}
 	Attribute& operator=(const Attribute& att){
 		table_id_=att.table_id_;
-		if(att.table_id_!=-1){
-			attrName=att.attrName;
+		attrName=att.attrName;
+		if(att.table_id_<ATTRIBUTE_ANY){
 			attrType=new column_type(*att.attrType);
 			index=att.index;
 		}
 		return *this;
 	}
 
-	Attribute():table_id_(-1){
+	Attribute(unsigned symbol=ATTRIBUTE_NULL):table_id_(symbol),index(-1),attrType(0){
+		switch(symbol){
+		case ATTRIBUTE_ANY:{
+			attrName="*";
+			break;
+		}
+		case ATTRIBUTE_NULL:{
+			attrName="NULL";
+			break;
+		}
+		default:{
+			assert(false);
+		}
+		}
 
 	}
 	~Attribute(){
-		attrType->~column_type();
+		if(table_id_<TableID(ATTRIBUTE_ANY)){
+			attrType->~column_type();
+		}
 	}
 	bool operator==(const Attribute& r)const{
 		return table_id_==r.table_id_&&index==r.index;
@@ -53,7 +71,13 @@ struct Attribute
 		return !(table_id_==r.table_id_&&index==r.index);
 	}
 	bool isNULL()const{
-		return table_id_==TableID(-1);
+		return table_id_==TableID(ATTRIBUTE_NULL);
+	}
+	bool isANY()const{
+		return table_id_==TableID(ATTRIBUTE_ANY);
+	}
+	std::string getName()const{
+		return attrName;
 	}
 	std::string attrName;
 	column_type* attrType;
