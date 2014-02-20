70febade0eb43f2b993bf9ff5e26d119ed8b1124
egraldlo <egraldlo@gmail.com>
2014-02-20 14:47:57 +0800

    begin to write the hashtable2 supported by variable version

---
+++ utility/hashtable2.h
@@ -0,0 +1,18 @@
+/*
+ * hashtable2.h
+ *
+ *  Created on: 2014-2-20
+ *      Author: casa
+ */
+
+#ifndef HASHTABLE2_H_
+#define HASHTABLE2_H_
+
+class hashtable2 {
+public:
+	hashtable2(unsigned nbuckets,unsigned bucksize,Schema *schema);
+	virtual ~hashtable2();
+
+};
+
+#endif /* HASHTABLE2_H_ */
