5bd259dbc3e838741369dd5a0a48d708b68dce1c
wangli1426 <wangli1426@gmail.com>
2014-02-21 09:54:11 +0800

    daemon, prepaired for client

---
+++ Daemon/Executing.h
@@ -0,0 +1,19 @@
+/*
+ * Executing.h
+ *
+ *  Created on: Feb 20, 2014
+ *      Author: wangli
+ */
+
+#ifndef EXECUTING_H_
+#define EXECUTING_H_
+#include <string>
+#include "../Block/ResultSet.h"
+class Executing {
+public:
+	Executing();
+	virtual ~Executing();
+	static ResultSet* run_sql(std::string sql);
+};
+
+#endif /* EXECUTING_H_ */
---
+++ Daemon/Test/daemon_test.h
@@ -0,0 +1,17 @@
+/*
+ * daemon_test.h
+ *
+ *  Created on: Feb 21, 2014
+ *      Author: wangli
+ */
+
+#ifndef DAEMON_TEST_H_
+#define DAEMON_TEST_H_
+#include "Executing_test.cpp"
+static int daemon_test(){
+	return execting_test();
+}
+
+
+
+#endif /* DAEMON_TEST_H_ */
