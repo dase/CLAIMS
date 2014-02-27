9b7f9e4ab32e7fb7115858f31ef643d82f2560af
wangli1426 <wangli1426@gmail.com>
2014-02-27 09:01:21 +0800

    a synchronizing point for zhuhe and kai

---
+++ common/TypePromotionMap.cpp
@@ -0,0 +1,12 @@
+/*
+ * TypePromotionMap.cpp
+ *
+ *  Created on: Feb 26, 2014
+ *      Author: wangli
+ */
+
+#include "TypePromotionMap.h"
+
+data_type TypePromotion::arith_type_promotion_map[DATA_TYPE_NUMBER][DATA_TYPE_NUMBER];
+
+
