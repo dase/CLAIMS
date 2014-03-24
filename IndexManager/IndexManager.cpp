73c6281acc7984bb8baa5c29eb3719082ce8e199
scdong <scdong510@163.com>
2014-03-24 10:52:15 +0800

    Copy before changing the CSBNode class into non-template

--- IndexManager/IndexManager.cpp
+++ IndexManager/IndexManager.cpp
@@ -9,10 +9,28 @@
 
 IndexManager::IndexManager() {
 	// TODO Auto-generated constructor stub
-
+	csb_index_.clear();
 }
 
 IndexManager::~IndexManager() {
 	// TODO Auto-generated destructor stub
 }
 
+IndexManager* IndexManager::getInstance()
+{
+	if (instance_ == 0)
+		instance_ = new BufferManager();
+	return instance_;
+}
+
+template <typename T>
+std::map<ChunkID, CSBPlusTree<T>* > IndexManager::getIndexList()
+{
+
+}
+
+template <typename T>
+bool IndexManager::addIndexToList(chunk_index_list<T> new_index)
+{
+
+}
--- IndexManager/IndexManager.h
+++ IndexManager/IndexManager.h
@@ -7,12 +7,43 @@
 
 #ifndef INDEXMANAGER_H_
 #define INDEXMANAGER_H_
+#include <map>
+#include <vector>
+#include "../ids.h"
+#include "CSBPlusTree.h"
+#include "../Catalog/Attribute.h"
+#include "../data_type.h"
 
+template <typename T>
+struct chunk_index_list
+{
+	Attribute attribute;
+	std::map<ChunkID, CSBPlusTree<T>* > csb_tree_list;
+//	ChunkID chunk_id;
+//	CSBPlusTree<T>* csb_tree;
+};
 
 class IndexManager {
 public:
-	IndexManager();
+	static IndexManager* getInstance();
 	virtual ~IndexManager();
+
+	template <typename T>
+	std::map<ChunkID, CSBPlusTree<T>* > getIndexList();
+
+	template <typename T>
+	bool addIndexToList(chunk_index_list<T> new_index);
+
+private:
+	IndexManager();
+
+private:
+//	template<typename T>
+//	map<Attribute, chunk_index_list<T> > csb_index_;
+	template<typename T>
+	vector<chunk_index_list<T> > csb_index_;
+
+	static IndexManager* instance_;
 };
 
 #endif /* INDEXMANAGER_H_ */
