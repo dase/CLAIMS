93671f6643841a8567ef8fcaef223d432a963861
wangli1426 <wangli1426@gmail.com>
2014-03-28 01:46:11 +0800

    Add Expanding and Shrinking derive class
    Former-commit-id: a06186fc987c32c2bca1644e6d4aba74a994de0d

---
+++ utility/ExpandabilityShrinkability.h
@@ -0,0 +1,19 @@
+/*
+ * ExpandabilityShrinkability.h
+ *
+ *  Created on: Mar 27, 2014
+ *      Author: wangli
+ */
+
+#ifndef EXPANDABILITYSHRINKABILITY_H_
+#define EXPANDABILITYSHRINKABILITY_H_
+
+class ExpandabilityShrinkability {
+public:
+	ExpandabilityShrinkability();
+	virtual ~ExpandabilityShrinkability();
+	virtual bool Expand()=0;
+	virtual bool Shrink()=0;
+};
+
+#endif /* EXPANDABILITYSHRINKABILITY_H_ */
