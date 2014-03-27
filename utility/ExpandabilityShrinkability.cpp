8f156675230e7f8328e9abbaa6a4a985cbfe5c1e
wangli1426 <wangli1426@gmail.com>
2014-03-28 01:46:11 +0800

    Add Expanding and Shrinking derive class
    Former-commit-id: a06186fc987c32c2bca1644e6d4aba74a994de0d

---
+++ utility/ExpandabilityShrinkability.cpp
@@ -0,0 +1,18 @@
+/*
+ * ExpandabilityShrinkability.cpp
+ *
+ *  Created on: Mar 27, 2014
+ *      Author: wangli
+ */
+
+#include "ExpandabilityShrinkability.h"
+
+ExpandabilityShrinkability::ExpandabilityShrinkability() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ExpandabilityShrinkability::~ExpandabilityShrinkability() {
+	// TODO Auto-generated destructor stub
+}
+
