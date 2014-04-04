28182ec6e7a99bd7c4dc2bb392544a032ae4a2c8
wangli1426 <wangli1426@gmail.com>
2014-04-04 17:33:21 +0800

    Fixed bugs in projectionScan and remove the codes in storage that limit the concurrency.
    Former-commit-id: c340324dd89a752baed4d912231c090d8e3e18f7

--- storage/ChunkStorage.h
+++ storage/ChunkStorage.h
@@ -16,28 +16,139 @@
 #include "../utility/lock.h"
 class ChunkReaderIterator{
 public:
-	ChunkReaderIterator(const ChunkID& chunk_id,const unsigned& number_of_blocks=0):chunk_id_(chunk_id),number_of_blocks_(number_of_blocks),cur_block_(0){};
+	/**
+	 * This structure maintains all the information needed to access
+	 *  a block in in-memory chunk, in-disk chunk, or in-hdfs chunk.
+	 *
+	 *  The underlying reason for using this structure is to.
+	 */
+	class block_accessor{
+	public:
+		virtual void getBlock(BlockStreamBase* &block) const {
+			assert(false);
+		}
+		unsigned getBlockSize() const {
+			return block_size;
+		}
+
+		void setBlockSize(unsigned blockSize) {
+			block_size = blockSize;
+		}
+
+		;
+	protected:
+		unsigned block_size;
+
+	};
+	class InMemeryBlockAccessor:public block_accessor{
+	public:
+		void getBlock(BlockStreamBase* &block)const;
+
+		void* getTargetBlockStartAddress() const {
+			return target_block_start_address;
+		}
+
+		void setTargetBlockStartAddress(void* targetBlockStartAddress) {
+			target_block_start_address = targetBlockStartAddress;
+		}
+
+	private:
+		void* target_block_start_address;
+	};
+
+	class InDiskBlockAccessor:public block_accessor{
+	public:
+		void getBlock(BlockStreamBase* &block)const;
+
+		unsigned getBlockCur() const {
+			return block_cur;
+		}
+
+		void setBlockCur(unsigned blockCur) {
+			block_cur = blockCur;
+		}
+
+		const ChunkID& getChunkId() const {
+			return chunk_id;
+		}
+
+		void setChunkId(const ChunkID& chunkId) {
+			chunk_id = chunkId;
+		}
+
+		unsigned getChunkSize() const {
+			return chunk_size;
+		}
+
+		void setChunkSize(unsigned chunkSize) {
+			chunk_size = chunkSize;
+		}
+
+	private:
+		unsigned chunk_size;
+		ChunkID chunk_id;
+		unsigned block_cur;
+	};
+
+
+	class InHDFSBlockAccessor:public block_accessor{
+	public:
+		void getBlock(BlockStreamBase* &block)const;
+
+		unsigned getBlockCur() const {
+			return block_cur;
+		}
+
+		void setBlockCur(unsigned blockCur) {
+			block_cur = blockCur;
+		}
+
+		const ChunkID& getChunkId() const {
+			return chunk_id;
+		}
+
+		void setChunkId(const ChunkID& chunkId) {
+			chunk_id = chunkId;
+		}
+
+		unsigned getChunkSize() const {
+			return chunk_size;
+		}
+
+		void setChunkSize(unsigned chunkSize) {
+			chunk_size = chunkSize;
+		}
+
+	private:
+		unsigned chunk_size;
+		ChunkID chunk_id;
+		unsigned block_cur;
+	};
+
+
+
+	ChunkReaderIterator(const ChunkID& chunk_id,unsigned block_size, unsigned chunk_size,const unsigned& number_of_blocks=0):chunk_id_(chunk_id),number_of_blocks_(number_of_blocks),cur_block_(0),block_size_(block_size),chunk_size_(chunk_size){};
 	virtual bool nextBlock(BlockStreamBase* & block)=0;
-
+	virtual bool getNextBlockAccessor(block_accessor* & ba)=0;
 	bool nextBlock();
-
 	virtual ~ChunkReaderIterator(){};
 public:
 	ChunkID chunk_id_;
 	unsigned number_of_blocks_;
 	unsigned cur_block_;
 	Lock lock_;
+	unsigned block_size_;
+	unsigned chunk_size_;
 };
 class InMemoryChunkReaderItetaor:public ChunkReaderIterator{
 public:
 	InMemoryChunkReaderItetaor(void* const &start,const unsigned& chunk_size,const unsigned & number_of_blocks,const unsigned& block_size,const ChunkID& chunk_id);
 	virtual ~InMemoryChunkReaderItetaor();
 	bool nextBlock(BlockStreamBase*& block);
+	bool getNextBlockAccessor(block_accessor* & ba);
 private:
 	void* start_;
-	unsigned chunk_size_;
 
-	unsigned block_size_;
 
 
 
@@ -48,10 +159,9 @@ public:
 	DiskChunkReaderIteraror(const ChunkID& chunk_id,unsigned& chunk_size,const unsigned& block_size);
 	virtual ~DiskChunkReaderIteraror();
 	bool nextBlock(BlockStreamBase*& block);
+	bool getNextBlockAccessor(block_accessor* & ba);
 private:
 
-	unsigned chunk_size_;
-	unsigned block_size_;
 //	unsigned number_of_blocks_;
 //	unsigned cur_block_;
 	/*the iterator creates a buffer and allocates its memory such that the query processing
@@ -67,10 +177,9 @@ public:
 	HDFSChunkReaderIterator(const ChunkID& chunk_id, unsigned& chunk_size,const unsigned& block_size);
 	virtual ~HDFSChunkReaderIterator();
 	bool nextBlock(BlockStreamBase*& block);
+	bool getNextBlockAccessor(block_accessor* & ba);
 private:
 
-	unsigned chunk_size_;
-	unsigned block_size_;
 //	unsigned number_of_blocks_;
 //	unsigned cur_block_;
 	/*the iterator creates a buffer and allocates its memory such that the query processing
