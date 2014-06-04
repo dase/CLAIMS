6a904ea0606a31b42db88eab81c45acaaa7a0b0f
wangli1426 <wangli1426@gmail.com>
2014-06-04 23:39:16 +0800

    In implementing insert-optimized hash table.
    Former-commit-id: 1b86c43177fddb980c93241be4e5847285811b52

---
+++ common/InsertOptimizedHashTable.cpp
@@ -0,0 +1,20 @@
+/*
+ * InsertOptimizedHashTable.cpp
+ *
+ *  Created on: Jun 4, 2014
+ *      Author: wangli
+ */
+
+#include "InsertOptimizedHashTable.h"
+
+InsertOptimizedHashTable::InsertOptimizedHashTable() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InsertOptimizedHashTable::~InsertOptimizedHashTable() {
+	// TODO Auto-generated destructor stub
+}
+
+void InsertOptimizedHashTable::insertBlock(BlockStreamBase* block) {
+}
