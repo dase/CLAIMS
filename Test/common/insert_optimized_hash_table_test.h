620621bf3c41f1576566a9b61a26b20d621500b6
wangli1426 <wangli1426@gmail.com>
2014-06-05 21:28:36 +0800

    InsertOptimizedHashTable has passed parallel inserting test. -_-
    Former-commit-id: 6e90cb6f17a7d5f6270f66c2e96ab0e02a21910f

--- Test/common/insert_optimized_hash_table_test.h
+++ Test/common/insert_optimized_hash_table_test.h
@@ -12,10 +12,33 @@
 #include <gtest/gtest.h>
 #include "../../common/InsertOptimizedHashTable.h"
 #include "../../utility/generator.h"
+#include <boost/functional/hash.hpp>
 
 static int insert_optimziaed_hash_table_test(){
 	InsertOptimizedHashTable::bucket t;
 	printf("size is %d\n",sizeof(t));
+}
+
+struct parallel_insert_args{
+	DynamicBlockBuffer::Iterator* buffer_it;
+	InsertOptimizedHashTable* hashtable;
+	unsigned nbuckets;
+};
+void* insert_into_insert_optimized_hash_table(void* args){
+	parallel_insert_args* arg=(parallel_insert_args*)args;
+
+	BlockStreamBase* block;
+
+	while((block=arg->buffer_it->atomicNextBlock())!=0){
+		BlockStreamBase::BlockStreamTraverseIterator* bit=block->createIterator();
+		arg->hashtable->insertBlock(block);
+		void* tuple;
+		while((tuple=bit->nextTuple())!=0){
+			InsertOptimizedHashTable::entry ent_tmp=InsertOptimizedHashTable::entry(tuple);
+			arg->hashtable->insertEntry(boost::hash_value(*(int*)tuple)%(arg->nbuckets),ent_tmp);
+		}
+		delete bit;
+	}
 }
 
 TEST(INSERT_OPTIMIZED_HASH_TABLE,INSERT){
@@ -24,7 +47,6 @@ TEST(INSERT_OPTIMIZED_HASH_TABLE,INSERT){
 	const unsigned tuple_length=32;
 
 	InsertOptimizedHashTable hashtable(nbuckets);
-	void* tmp;
 
 	Schema* schema=generateSchema(tuple_length);
 	DynamicBlockBuffer* buffer=generate_BlockStreamBuffer(schema,data_size_in_MB);
@@ -33,15 +55,58 @@ TEST(INSERT_OPTIMIZED_HASH_TABLE,INSERT){
 
 	BlockStreamBase* block;
 
+	unsigned long long int start=curtick();
 	while((block=it.nextBlock())!=0){
 		BlockStreamBase::BlockStreamTraverseIterator* bit=block->createIterator();
 		hashtable.insertBlock(block);
 		void* tuple;
 		while((tuple=bit->nextTuple())!=0){
 			InsertOptimizedHashTable::entry ent_tmp=InsertOptimizedHashTable::entry(tuple);
-			hashtable.insertEntry(random()%(nbuckets),ent_tmp);
+			hashtable.insertEntry(boost::hash_value(*(int*)tuple)%(nbuckets),ent_tmp);
 		}
 		delete bit;
+	}
+	unsigned long long int total_cycles=curtick()-start;
+
+	unsigned long tuples_in_hashtable=0;
+	for(unsigned i=0;i<nbuckets;i++){
+		InsertOptimizedHashTable::BucketIterator buck_iter=hashtable.createBucketIterator(i);
+		while(buck_iter.nextTuple()){
+			tuples_in_hashtable++;
+		}
+	}
+	printf("%d cycles per tuple!\n",total_cycles/tuples_in_hashtable);
+
+	EXPECT_EQ(buffer->getNumberOftuples(),tuples_in_hashtable);
+	delete schema;
+	delete buffer;
+}
+
+TEST(INSERT_OPTIMIZED_HASH_TABLE,INSERT_PARALLEL){
+	const unsigned nbuckets=1024;
+	const unsigned data_size_in_MB=1024;
+	const unsigned tuple_length=32;
+	const unsigned nthreads=4;
+
+	InsertOptimizedHashTable hashtable(nbuckets);
+
+	Schema* schema=generateSchema(tuple_length);
+	DynamicBlockBuffer* buffer=generate_BlockStreamBuffer(schema,data_size_in_MB);
+
+	DynamicBlockBuffer::Iterator it=buffer->createIterator();
+
+	parallel_insert_args para;
+	para.buffer_it=&it;
+	para.hashtable=&hashtable;
+	para.nbuckets=nbuckets;
+	pthread_t pid[20];
+	for(unsigned i=0;i<nthreads;i++){
+
+		pthread_create(&pid[i],0,insert_into_insert_optimized_hash_table,&para);
+	}
+
+	for(unsigned i=0;i<nthreads;i++){
+		pthread_join(pid[i],0);
 	}
 
 	unsigned long tuples_in_hashtable=0;
