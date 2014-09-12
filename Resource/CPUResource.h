e7f58dfde064dc2165dba0fa57e2b40a3378602d
wangli1426 <wangli1426@gmail.com>
2014-09-12 21:27:31 +0800

    Continue CPU resource
    Former-commit-id: 8605da1dba4ba3bac79912a7fda5745625291045

--- Resource/CPUResource.h
+++ Resource/CPUResource.h
@@ -9,18 +9,31 @@
 #define CPURESOURCE_H_
 #include <vector>
 
-class CPUResource {
+class CPUResourceManager {
 public:
 	struct socket{
 		int cores;
 		int busy_cores;
+		semaphore sem_cores;
+		socket(int number_of_cores);
 		int getNumberOfFreeCores();
-		bool applyCore();
+		/* apply for a core, return false if no cores is currently available.*/
+		bool tryApplyCore();
+		/* apply for a core. This function will be blocked until a core is
+		 * successfully obtained.
+		 */
+		void applyCore();
+		/* return a core to the CPU resource manager*/
+		void returnCore();
 	};
-	CPUResource();
-	virtual ~CPUResource();
-	/** apply for a core available on any socket **/
-	bool applyCore();
+	CPUResourceManager();
+	virtual ~CPUResourceManager();
+	/** apply for a core available on any socket,
+	 * this function returns false when there is no cores available. **/
+	bool tryApplyCore();
+
+	/** apply for a core, blocked if no cores are available.*/
+	void applyCore();
 
 	/** return the core to the Cpu resource
 	 *  socket_index should be given if the caller is
