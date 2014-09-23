2322808504b33aa54c5dab09d317a5fabc399493
wangli1426 <wangli1426@gmail.com>
2014-09-23 06:18:53 -0400

    Add partition_schema, which covers hash, range, and boardcast partitioning.
    Former-commit-id: a7f8a0577412e3d23f8288c716270c5fce37510d

---
+++ common/partition_functions.h
@@ -0,0 +1,49 @@
+/*
+ * partition_functions.h
+ *
+ *  Created on: Sep 22, 2014
+ *      Author: wangli
+ */
+
+#ifndef PARTITION_FUNCTIONS_H_
+#define PARTITION_FUNCTIONS_H_
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/utility.hpp>
+
+struct partition_schema{
+	enum partition_mode{hash,broadcase,range};
+	partition_mode mode;
+	unsigned partition_key_index;
+	static partition_schema set_hash_partition(int _partition_key_index=0){
+		partition_schema ret;
+		ret.mode=hash;
+		ret.partition_key_index=_partition_key_index;
+		return ret;
+	}
+	static partition_schema set_broadcast_partition(){
+		partition_schema ret;
+		ret.mode=broadcase;
+		ret.partition_key_index=0;
+		return ret;
+	}
+	static partition_schema set_range_partition(int _number_of_ranges){
+		assert(false);
+		return partition_schema();
+	}
+	bool isHashPartition()const{
+		return mode==hash;
+	}
+	bool isBoardcastPartition()const{
+		return mode==broadcase;
+	}
+
+	friend class boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version){
+		ar & mode & partition_key_index;
+	}
+};
+
+#endif /* PARTITION_FUNCTIONS_H_ */
