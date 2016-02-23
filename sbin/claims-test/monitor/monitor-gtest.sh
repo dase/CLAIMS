6be0cf0d72bad8d13c0902721b962e16a2316f65
cs-wang <cs_wang@infosys.com>
2016-02-23 21:56:46 +0800

    add scripts for test

---
+++ sbin/claims-test/monitor/monitor-gtest.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+cd $CLAIMS_HOME/sbin/2-claims-conf
+source ./load-config.sh
+
+while [ 1 ]
+do
+ procid=`pgrep claimsserver`
+ if [ "$procid" = "" ]; then
+  echo "claimsserver is aborted. Try to restart..."
+  cd $CLAIMS_HOME/sbin
+  ./4-stop-all.sh
+  if [ -d "$CLAIMS_HOME/install" ]; then
+    cd $CLAIMS_HOME/install
+    if [ ! -f "claimsserver" ]; then
+     cd $CLAIMS_HOME/sbin
+     ./1-compile.sh
+    fi
+  else
+    cd $CLAIMS_HOME/sbin
+    ./1-compile.sh
+  fi 
+  cd $CLAIMS_HOME/sbin
+  ./5-start-all.sh
+  sleep 3 
+  cd $CLAIMS_HOME/sbin/claims-test
+  ./claimstest.sh 1 1 load_tpch_sf1_1p
+ else
+  echo "claimsserver is running..."
+  $CLAIMS_HOME/install/test --ip $master --port $client_listener_port
+ fi
+done
