f75335264b39f891f7bfa2225a937f9e7b298a36
cs-wang <cs_wang@infosys.com>
2016-03-15 22:05:24 +0800

    support to start cluster from any node

--- sbin/claims-test/claimstest.sh
+++ sbin/claims-test/claimstest.sh
@@ -3,12 +3,11 @@ set -e
 cd $CLAIMS_HOME/sbin/2-claims-conf/
 source ./load-config.sh
 
-##################
-# start test     #
-##################
-  cd $CLAIMS_HOME
-  cd install
-  ulimit -c unlimited
+# for debug begin #####
+cd $CLAIMS_HOME
+cd install
+ulimit -c unlimited
+# for debug end #######
 
 if [ "$1" = "" ]; then
   runloops=1
@@ -26,16 +25,15 @@ if [ "$3" = "" ]; then
  echo "please input test case as the third param."
  exit 1
 fi
-
 echo "runloops=[$runloops]"
 echo "concurrency_count=[$concurrency_count]"
 
-thislog=$logfilepath/client.$(date +%Y-%m-%d).log
-
-testmode="userresult"
-
-if [ "$testmode" = "userresult" ]; then
+thislog=$logpath/client.$(date +%Y-%m-%d).log
 cd $CLAIMS_HOME/sbin/claims-test
+if [ ! -d "testresult" ]; then
+ mkdir -p "testresult"
+fi
+
 for((loop=1;loop<=runloops;loop++))
 do
 {
@@ -43,35 +41,12 @@ do
  do
  {
   datestr=`date '+%Y-%m-%d %H:%M:%S'`
-  thisstartstr="========run test:[$3] [$loop-$cur] time: $datestr========"
+  thisstartstr="========run test:[$3] [$loop-$cur] time:[$datestr]========"
   echo -e "\033[33m$thisstartstr\033[0m"
-#  echo $thisstartstr >> $thislog
   $CLAIMS_HOME/install/client $master $client_listener_port < $CLAIMS_HOME/sbin/claims-test/testcase/$3.test > $CLAIMS_HOME/sbin/claims-test/testresult/$3-$loop-$cur.result
   sleep 1
-
  }&
  done
  wait
 }
 done
-else
-for((loop=1;loop<=runloops;loop++))
-do
-{
- for((cur=1;cur<=concurrency_count;cur++))  
- do
- {
-
-  datestr=`date '+%Y-%m-%d %H:%M:%S'`
-  thisstartstr="========run client [$loop-$cur] time: $datestr========"
-  echo -e "\033[33m$thisstartstr\033[0m"
-  echo $thisstartstr >> $thislog
-  ./client 127.0.0.1 11000 < /home/claims/jenkins-scripts/claims-test/testcase/decimal.test >> $thislog
-  sleep 1
-
- }&
- done
- wait
-}
-done
-fi
---
+++ sbin/start-all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh  
+
+if [ ! -f "${0##*/}" ]; then
+ echo "please run script in sbin/ directory!"
+ exit 1
+fi
+
+cd $CLAIMS_HOME/sbin/2-claims-conf/
+source ./load-config.sh
+source ./generate-config.sh
+  
+# start cluster
+for node in $master $slaves
+do
+{
+#  ssh -f -n -l $user $node "$claimshome/sbin/start-node.sh $claimshome/sbin/2-claims-conf/config-$node>/dev/null 2>&1" & 
+  ssh -f -n -l $user $node "$claimshome/sbin/start-node.sh $claimshome/sbin/2-claims-conf/config-$node" & 
+#  echo -e "$node claimsserver start [\033[32mOK\033[0m]"
+}
+done
+
