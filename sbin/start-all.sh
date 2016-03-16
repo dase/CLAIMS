0104fb23e1650e9d216fdb7d1e6e7232b98cef2b
cs-wang <cs_wang@infosys.com>
2016-03-16 11:11:28 +0800

    support to start claims on any node in cluster

--- sbin/start-all.sh
+++ sbin/start-all.sh
@@ -13,9 +13,8 @@ source ./generate-config.sh
 for node in $master $slaves
 do
 {
-#  ssh -f -n -l $user $node "$claimshome/sbin/start-node.sh $claimshome/sbin/2-claims-conf/config-$node>/dev/null 2>&1" & 
-  ssh -f -n -l $user $node "$claimshome/sbin/start-node.sh $claimshome/sbin/2-claims-conf/config-$node" & 
-#  echo -e "$node claimsserver start [\033[32mOK\033[0m]"
-}
+  ssh -f -n -l $user $node "$claimshome/sbin/start-node.sh $claimshome/sbin/2-claims-conf/config-$node>/dev/null 2>&1"
+  echo -e "$node claimsserver start [\033[32mOK\033[0m]"
+}&
 done
-
+wait
