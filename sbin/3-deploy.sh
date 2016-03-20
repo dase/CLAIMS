c08c1c897531376ad7471cc9fa0a4054b8bd1f71
cs-wang <cs_wang@infosys.com>
2016-03-20 13:20:06 +0800

    config and deploy improvement

--- sbin/3-deploy.sh
+++ sbin/3-deploy.sh
@@ -17,5 +17,10 @@ do
   scp $CLAIMS_HOME/install/claimsserver $user@$node:$claimshome/install
   scp $CLAIMS_HOME/install/client $user@$node:$claimshome/install
   scp $CLAIMS_HOME/install/test $user@$node:$claimshome/install
-  scp -r $CLAIMS_HOME/sbin $user@$node:$claimshome
+  scp -r $CLAIMS_HOME/sbin/2-claims-conf $user@$node:$claimshome/sbin
+  scp $CLAIMS_HOME/sbin/*.sh $user@$node:$claimshome/sbin
+  scp -r $CLAIMS_HOME/sbin/claims-test/testcase $user@$node:$claimshome/sbin/claims-test
+  scp -r $CLAIMS_HOME/sbin/claims-test/monitor $user@$node:$claimshome/sbin/claims-test
+  scp -r $CLAIMS_HOME/sbin/claims-test/*.sh $user@$node:$claimshome/sbin/claims-test
+
 done
